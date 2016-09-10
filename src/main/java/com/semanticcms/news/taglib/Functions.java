335cdbcd1234b7ea83c9a43ab7dfac438f719397
AO Industries, Inc. <support@aoindustries.com>
2016-09-09 20:28:53 -0500

    Adding newsfeeds

---
+++ src/main/java/com/semanticcms/news/taglib/Functions.java
@@ -0,0 +1,51 @@
+/*
+ * semanticcms-news-taglib - SemanticCMS newsfeeds in a JSP environment.
+ * Copyright (C) 2016  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of semanticcms-news-taglib.
+ *
+ * semanticcms-news-taglib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * semanticcms-news-taglib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with semanticcms-news-taglib.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.semanticcms.news.taglib;
+
+import static com.aoindustries.servlet.filter.FunctionContext.getRequest;
+import static com.aoindustries.servlet.filter.FunctionContext.getResponse;
+import static com.aoindustries.servlet.filter.FunctionContext.getServletContext;
+import com.semanticcms.core.model.Page;
+import com.semanticcms.news.model.News;
+import com.semanticcms.news.servlet.NewsUtils;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+
+final public class Functions {
+
+	public static List<News> findAllNews(Page page) throws ServletException, IOException {
+		return NewsUtils.findAllNews(
+			getServletContext(),
+			getRequest(),
+			getResponse(),
+			page
+		);
+	}
+
+	/**
+	 * Make no instances.
+	 */
+	private Functions() {
+	}
+}

630655dc6132cb82aec44ec6cfc533a86c95a33b
AO Industries, Inc <support@aoindustries.com>
2019-04-15 20:51:40 -0500

    Using Maven property "documented.javac.link.javaApi" to link to the correct Java API version

--- book/src/main/java/com/semanticcms/news/taglib/book/SemanticCmsNewsTldInitializer.java
+++ book/src/main/java/com/semanticcms/news/taglib/book/SemanticCmsNewsTldInitializer.java
@@ -42,7 +42,7 @@ public class SemanticCmsNewsTldInitializer extends TagReferenceInitializer {
 			"Taglib Reference",
 			"/news/taglib",
 			"/semanticcms-news.tld",
-			Maven.properties.getProperty("javac.link.javaApi.jdk7"),
+			Maven.properties.getProperty("documented.javac.link.javaApi"),
 			Maven.properties.getProperty("javac.link.javaeeApi.6"),
 			additionalApiLinks
 		);

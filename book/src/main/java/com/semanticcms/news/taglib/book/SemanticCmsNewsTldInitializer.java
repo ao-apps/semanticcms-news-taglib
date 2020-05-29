9d3b01cdf4ca7a84e717dd083eddfbf5f2936785
AO Industries, Inc <support@aoindustries.com>
2020-05-29 18:29:48 -0500

    Trailing dot no longer required

--- book/src/main/java/com/semanticcms/news/taglib/book/SemanticCmsNewsTldInitializer.java
+++ book/src/main/java/com/semanticcms/news/taglib/book/SemanticCmsNewsTldInitializer.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-news-taglib - SemanticCMS newsfeeds in a JSP environment.
- * Copyright (C) 2016, 2017, 2019  AO Industries, Inc.
+ * Copyright (C) 2016, 2017, 2019, 2020  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -23,18 +23,9 @@
 package com.semanticcms.news.taglib.book;
 
 import com.semanticcms.tagreference.TagReferenceInitializer;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.Collections;
 
 public class SemanticCmsNewsTldInitializer extends TagReferenceInitializer {
-
-	private static final Map<String,String> additionalApiLinks = new LinkedHashMap<>();
-	static {
-		// Self
-		additionalApiLinks.put("com.semanticcms.news.taglib.", Maven.properties.getProperty("project.url") + "apidocs/");
-		// Dependencies
-		additionalApiLinks.put("com.semanticcms.core.model.", "https://semanticcms.com/core/model/apidocs/");
-	}
 
 	public SemanticCmsNewsTldInitializer() {
 		super(
@@ -44,7 +35,10 @@ public class SemanticCmsNewsTldInitializer extends TagReferenceInitializer {
 			"/semanticcms-news.tld",
 			Maven.properties.getProperty("documented.javadoc.link.javase"),
 			Maven.properties.getProperty("documented.javadoc.link.javaee"),
-			additionalApiLinks
+			// Self
+			Collections.singletonMap("com.semanticcms.news.taglib", Maven.properties.getProperty("project.url") + "apidocs/"),
+			// Dependencies
+			Collections.singletonMap("com.semanticcms.core.model", "https://semanticcms.com/core/model/apidocs/")
 		);
 	}
 }

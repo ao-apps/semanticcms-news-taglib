747f731c294868bcc91739067345f7b91701b8d5
AO Industries, Inc <support@aoindustries.com>
2020-06-03 17:14:00 -0500

    Now using String varargs instead of generic Map

--- book/src/main/java/com/semanticcms/news/taglib/book/SemanticCmsNewsTldInitializer.java
+++ book/src/main/java/com/semanticcms/news/taglib/book/SemanticCmsNewsTldInitializer.java
@@ -23,11 +23,9 @@
 package com.semanticcms.news.taglib.book;
 
 import com.semanticcms.tagreference.TagReferenceInitializer;
-import java.util.Collections;
 
 public class SemanticCmsNewsTldInitializer extends TagReferenceInitializer {
 
-	@SuppressWarnings("unchecked")
 	public SemanticCmsNewsTldInitializer() {
 		super(
 			Maven.properties.getProperty("project.name") + " Reference",
@@ -38,10 +36,10 @@ public class SemanticCmsNewsTldInitializer extends TagReferenceInitializer {
 			Maven.properties.getProperty("documented.javadoc.link.javase"),
 			Maven.properties.getProperty("documented.javadoc.link.javaee"),
 			// Self
-			Collections.singletonMap("com.semanticcms.news.taglib", Maven.properties.getProperty("project.url") + "apidocs/"),
+			"com.semanticcms.news.taglib", Maven.properties.getProperty("project.url") + "apidocs/",
 			// Dependencies
-			Collections.singletonMap("com.semanticcms.core.model", "https://semanticcms.com/core/model/apidocs/"),
-			Collections.singletonMap("com.semanticcms.news.model", "https://semanticcms.com/news/model/apidocs/")
+			"com.semanticcms.core.model", "https://semanticcms.com/core/model/apidocs/",
+			"com.semanticcms.news.model", "https://semanticcms.com/news/model/apidocs/"
 		);
 	}
 }

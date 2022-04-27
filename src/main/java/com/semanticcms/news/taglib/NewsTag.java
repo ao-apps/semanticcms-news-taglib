33f7e2527749db2eb7d26ea436d970e7c4f0a335
AO Industries, Inc <support@aoindustries.com>
2022-04-27 00:14:49 -0500

    Performed automatic clean-up with org.openrewrite.java.cleanup.Cleanup

--- src/main/java/com/semanticcms/news/taglib/NewsTag.java
+++ src/main/java/com/semanticcms/news/taglib/NewsTag.java
@@ -145,7 +145,7 @@ public class NewsTag extends ElementTag<News> {
     NewsImpl.writeNewsImpl(
         request,
         new Document(serialization, doctype, characterEncoding, out)
-            .setAutonli(false) // Do not add extra newlines to JSP
+            .setAutonli(false)// Do not add extra newlines to JSP
             .setIndent(false), // Do not add extra indentation to JSP
         context,
         getElement(),
